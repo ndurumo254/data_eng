f5dea7220bc2ab834cbee51e065193f2ed4642d2
ndurumo254 <erickndurumo@gmail.com>
2025-05-26 13:18:06 +0300

    topic

---
+++ spark-jobs/main.py
@@ -0,0 +1,7 @@
+import os
+import json
+from confluent_kafka import SerializingProducer
+from confluent_kafka.serialization import StringSerializer
+
+Konstanz_city: {"latitude": 47.6779,"longitude": 9.1732}
+Stuttgart_city: { "latitude": 48.7758, "longitude": 9.1829}
